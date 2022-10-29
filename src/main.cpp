cf4bcb94656ce22eae8c121cbf2e0c9bbc74901f
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-10-29 22:47:04 +0800

    feat: add main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+#include "HelloWorldPrinter.hpp"
+
+int main()
+{
+  HelloWorldPrinter helloWorldPrinter = HelloWorldPrinter();
+  for (int i = 0; i < 1; i++) {
+    std::cout << helloWorldPrinter.makeHelloWorldString();
+  }
+
+  return 1;
+}
